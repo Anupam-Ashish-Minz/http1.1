ec48da4bab224221c023beae4fc5a302e699422a
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-23 17:27:22 +0530

    new assert macro with type info

--- test/char_array_test.c
+++ test/char_array_test.c
@@ -1,10 +1,22 @@
 #include "char_array.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define ASSERT_EQ(a, b)                                                        \
 	if (a != b) {                                                              \
 		fprintf(stderr, "Assert failed: %s, %s\n", #a, #b);                    \
+		exit(-1);                                                              \
+	}
+
+#define ASSERT_EQ_T(t, a, b)                                                   \
+	if (a != b) {                                                              \
+		fprintf(stderr, "Assert failed: %s, %s\n", #a, #b);                    \
+		if (strcmp(t, "int") == 0) {                                           \
+			fprintf(stderr, "Assert values: %d, %d\n", a, b);                  \
+		} else if (strcmp(t, "float") == 0) {                                  \
+			fprintf(stderr, "Assert values: %f, %f\n", a, b);                  \
+		}                                                                      \
 		exit(-1);                                                              \
 	}
 
