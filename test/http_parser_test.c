c3d021097263271fbd16e6d1ba5124de6cece6be
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-23 20:15:29 +0530

    basic line split

--- test/http_parser_test.c
+++ test/http_parser_test.c
@@ -1,5 +1,14 @@
 #include "http_parser.h"
+#include <string.h>
 
 int main(int argc, char **argv) {
+
+	char *request = "get / http/1.1\r\n\
+host: localhost:4000\r\n\
+user-agent: curl/8.6.0\r\n\
+Accept: */*\r\n";
+
+	parse_http(request, strlen(request));
+
 	return 0;
 }
