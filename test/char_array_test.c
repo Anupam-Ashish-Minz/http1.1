2489ae97271e18a52fa682c67ea088262cd8639e
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-23 17:01:08 +0530

    new assert_eq macro

--- test/char_array_test.c
+++ test/char_array_test.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ASSERT_EQ(a, b)                                                        \
+	if (a != b) {                                                              \
+		fprintf(stderr, "Assert failed: %s, %s\n", #a, #b);                    \
+		exit(-1);                                                              \
+	}
+
 void test_push_1_item() {
 	struct VecCharArray vec = new_vec_char_array();
 	struct CharArray *arr =
-	    (struct CharArray *)malloc(sizeof(struct CharArray));
+		(struct CharArray *)malloc(sizeof(struct CharArray));
 
 	push_char_array(&vec, arr);
-	if (vec.len != 1) {
-		fprintf(stderr, "vector length 1 != %d\n", vec.len);
-		exit(-1);
-	}
-	if (vec.max_len != 10) {
-		fprintf(stderr, "vector max length 10 != %d\n", vec.max_len);
-		exit(-1);
-	}
+	ASSERT_EQ(vec.len, 1);
+	ASSERT_EQ(vec.max_len, 10);
 }
 
 void test_push_101_item() {
 	struct VecCharArray vec = new_vec_char_array();
 	struct CharArray *arr =
-	    (struct CharArray *)malloc(sizeof(struct CharArray));
+		(struct CharArray *)malloc(sizeof(struct CharArray));
 
 	for (int i = 0; i < 102; i++) {
 		push_char_array(&vec, arr);
