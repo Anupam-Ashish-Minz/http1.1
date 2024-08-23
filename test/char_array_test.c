7d0b2d5dd379e70aaa33d56fc083169cb32c27b0
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-23 16:27:32 +0530

    formatting changes

--- test/char_array_test.c
+++ test/char_array_test.c
@@ -4,17 +4,14 @@
 
 void test_push_1_item() {
 	struct VecCharArray vec = new_vec_char_array();
-
 	struct CharArray *arr =
 	    (struct CharArray *)malloc(sizeof(struct CharArray));
 
 	push_char_array(&vec, arr);
-
 	if (vec.len != 1) {
 		fprintf(stderr, "vector length 1 != %d\n", vec.len);
 		exit(-1);
 	}
-
 	if (vec.max_len != 10) {
 		fprintf(stderr, "vector max length 10 != %d\n", vec.max_len);
 		exit(-1);
@@ -23,19 +20,16 @@ void test_push_1_item() {
 
 void test_push_101_item() {
 	struct VecCharArray vec = new_vec_char_array();
-
 	struct CharArray *arr =
 	    (struct CharArray *)malloc(sizeof(struct CharArray));
 
-	for (int i=0; i<102; i++) {
+	for (int i = 0; i < 102; i++) {
 		push_char_array(&vec, arr);
 	}
-
 	if (vec.len != 102) {
 		fprintf(stderr, "vector length 101 != %d\n", vec.len);
 		exit(-1);
 	}
-
 	if (vec.max_len != 110) {
 		fprintf(stderr, "vector max length 110 != %d\n", vec.max_len);
 		exit(-1);
