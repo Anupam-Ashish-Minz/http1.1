fd31f5a73a7d0e307320e7eebc31ab65b9c14ea4
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-24 11:43:22 +0530

    update assert function in test utils

--- test/utils.h
+++ test/utils.h
@@ -7,7 +7,8 @@
 
 #define ASSERT_EQ(a, b)                                                        \
 	if (a != b) {                                                              \
-		fprintf(stderr, "Assert failed: %s, %s\n", #a, #b);                    \
+		fprintf(stderr, "Assert failed: %s, %s\nvalues %d, %d\n", #a, #b, a,   \
+				b);                                                            \
 		exit(-1);                                                              \
 	}
 
