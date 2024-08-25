1c387432ba68cf5e8968dc5b4c1192910ca40979
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-25 10:01:00 +0530

    change variable names in char array test

--- test/char_array_test.c
+++ test/char_array_test.c
@@ -158,42 +158,42 @@ void test_split_line2() {
 	char *lines = "line number 1\r\n"
 		"line number 2";
 
-	char **linebuf = (char **)malloc(sizeof(lines));
-	size_t *linebuf_s = (size_t *)malloc(sizeof(lines));
+	char **buf = (char **)malloc(sizeof(lines));
+	size_t *buf_s = (size_t *)malloc(sizeof(lines) * sizeof(size_t));
 
-	int line_count = split_lines2(lines, strlen(lines), linebuf, linebuf_s);
+	int line_count = split_lines2(lines, strlen(lines), buf, buf_s);
 
 	ASSERT_EQ(line_count, 2);
-	ASSERT_EQ(linebuf_s[0], 13);
-	ASSERT_EQ(linebuf_s[1], 13);
+	ASSERT_EQ(buf_s[0], 13);
+	ASSERT_EQ(buf_s[1], 13);
 
-	ASSERT_EQ_STR(linebuf[0], "line number 1");
-	ASSERT_EQ_STR(linebuf[1], "line number 2");
+	ASSERT_EQ_STR(buf[0], "line number 1");
+	ASSERT_EQ_STR(buf[1], "line number 2");
 
-	free(linebuf);
-	free(linebuf_s);
+	free(buf);
+	// free(buf_s);
 }
 
 void test_split_black_line2() {
 	char *lines = "line number 1\r\n\r\n"
 		"line number 2";
 
-	char **linebuf = (char **)malloc(sizeof(lines));
-	size_t *linebuf_s = (size_t *)malloc(sizeof(lines));
+	char **buf = (char **)malloc(sizeof(lines));
+	size_t *buf_s = (size_t *)malloc(sizeof(lines) * sizeof(size_t));
 
-	int line_count = split_lines2(lines, strlen(lines), linebuf, linebuf_s);
+	int line_count = split_lines2(lines, strlen(lines), buf, buf_s);
 
 	ASSERT_EQ(line_count, 3);
-	ASSERT_EQ(linebuf_s[0], 13);
-	ASSERT_EQ(linebuf_s[1], 0);
-	ASSERT_EQ(linebuf_s[2], 13);
+	ASSERT_EQ(buf_s[0], 13);
+	ASSERT_EQ(buf_s[1], 0);
+	ASSERT_EQ(buf_s[2], 13);
 
-	ASSERT_EQ_STR(linebuf[0], "line number 1");
-	ASSERT_EQ_STR(linebuf[1], "");
-	ASSERT_EQ_STR(linebuf[2], "line number 2");
+	ASSERT_EQ_STR(buf[0], "line number 1");
+	ASSERT_EQ_STR(buf[1], "");
+	ASSERT_EQ_STR(buf[2], "line number 2");
 
-	free(linebuf);
-	free(linebuf_s);
+	free(buf);
+	// free(buf_s);
 }
 
 void test_split_multiple_lines2() {
@@ -201,23 +201,23 @@ void test_split_multiple_lines2() {
 		"line number 2\r\n\r\n"
 		"line number 3";
 
-	char **linebuf = (char **)malloc(sizeof(lines));
-	size_t *linebuf_s = (size_t *)malloc(sizeof(lines));
+	char **buf = (char **)malloc(sizeof(lines));
+	size_t *buf_s = (size_t *)malloc(sizeof(lines) * sizeof(size_t));
 
-	int line_count = split_lines2(lines, strlen(lines), linebuf, linebuf_s);
+	int line_count = split_lines2(lines, strlen(lines), buf, buf_s);
 
 	ASSERT_EQ(line_count, 4);
-	ASSERT_EQ(linebuf_s[0], 13);
-	ASSERT_EQ(linebuf_s[1], 13);
-	ASSERT_EQ(linebuf_s[2], 0);
-	ASSERT_EQ(linebuf_s[3], 13);
+	ASSERT_EQ(buf_s[0], 13);
+	ASSERT_EQ(buf_s[1], 13);
+	ASSERT_EQ(buf_s[2], 0);
+	ASSERT_EQ(buf_s[3], 13);
 
-	ASSERT_EQ_STR(linebuf[0], "line number 1");
-	ASSERT_EQ_STR(linebuf[1], "line number 2");
-	ASSERT_EQ_STR(linebuf[2], "");
-	ASSERT_EQ_STR(linebuf[3], "line number 3");
+	ASSERT_EQ_STR(buf[0], "line number 1");
+	ASSERT_EQ_STR(buf[1], "line number 2");
+	ASSERT_EQ_STR(buf[2], "");
+	ASSERT_EQ_STR(buf[3], "line number 3");
 
-	free(linebuf);
+	free(buf);
 	// free(linebuf_s); // I don't know why calling free on this causes double free failure
 }
 
