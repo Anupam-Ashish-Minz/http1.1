c3e5e3eb2bed9b988cea405f325a50c12e76ede0
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-26 21:38:41 +0530

    formatting http parser tests

--- test/http_parser_test.c
+++ test/http_parser_test.c
@@ -1,17 +1,17 @@
-#include "http_parser.h"
 #include "char_array.h"
+#include "http_parser.h"
 #include "utils.h"
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 void test_parse_simple_get_request() {
-	char *raw_req = "get / http/1.1\r\n"
-		"host: localhost:4000\r\n"
-		"user-agent: curl/8.6.0\r\n"
-		"Accept: */*\r\n";
+	char *raw = "get / http/1.1\r\n"
+				"host: localhost:4000\r\n"
+				"user-agent: curl/8.6.0\r\n"
+				"Accept: */*\r\n";
 
 	struct HttpRequest request;
-	int status = parse_http_request(raw_req, strlen(raw_req), &request);
+	int status = parse_http_request(raw, strlen(raw), &request);
 	if (status != 0) {
 		fprintf(stderr, "failed to parse to http request\n");
 		exit(-1);
@@ -23,12 +23,13 @@ void test_parse_simple_get_request() {
 
 void test_parse_post_request() {
 	char *raw = "POST / HTTP/1.1\r\n"
-		"Host: localhost:4000\r\n"
-		"User-Agent: curl/8.6.0\r\n"
-		"Accept: */*\r\n"
-		"Content-Type: application/json\r\n"
-		"Content-Length: 61\r\n\r\n"
-		"{\"name\": \"amy\", \"email\":\"alice@mail.com\", \"password\": \"pass\"}\r\n";
+				"Host: localhost:4000\r\n"
+				"User-Agent: curl/8.6.0\r\n"
+				"Accept: */*\r\n"
+				"Content-Type: application/json\r\n"
+				"Content-Length: 61\r\n\r\n"
+				"{\"name\": \"amy\", \"email\":\"alice@mail.com\", "
+				"\"password\": \"pass\"}\r\n";
 
 	struct HttpRequest request;
 	int status = parse_http_request(raw, strlen(raw), &request);
