0542681d9d08a1378cb30f2a53bb22c5eb445e76
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-08-26 12:51:24 +0530

    remove old char arrays

--- src/char_array.c
+++ src/char_array.c
@@ -3,107 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#if CHARARR == 1
-
-struct VecCharArray new_vec_char_array() {
-	struct VecCharArray arr = {
-	    NULL,
-	    0,
-	    0,
-	};
-	return arr;
-}
-
-void push_char_array(struct VecCharArray *vec, struct CharArray *arr) {
-	if (vec->arr == NULL) {
-		vec->arr = (struct CharArray **)malloc(
-		    10 * sizeof(struct CharArray *));
-		vec->len = 0;
-		vec->max_len = 10;
-	}
-	if (vec->len - 1 == vec->max_len) {
-		vec->arr = (struct CharArray **)realloc(
-		    vec->arr, (vec->max_len + 10) * sizeof(struct CharArray *));
-		vec->max_len += 10;
-	}
-	vec->arr[vec->len] = arr;
-	vec->len += 1;
-}
-
-struct VecCharArray split_lines(char *lines, size_t size) {
-	struct VecCharArray vlines = new_vec_char_array();
-	int prev = 0;
-
-	for (int i=0; i<size-1; i++) {
-		if (lines[i] == '\r' && lines[i+1] == '\n') {
-			struct CharArray *line = (struct CharArray *)malloc(sizeof(struct CharArray));
-			line->line = (char *)malloc(i - prev);
-			line->size = i - prev;
-			strncpy(line->line, &lines[prev], i - prev);
-			printf("->%s<-\n", line->line);
-			push_char_array(&vlines, line);
-			prev = i+2;
-		}
-	}
-	if (prev < size) {
-		struct CharArray *line = (struct CharArray *)malloc(sizeof(struct CharArray));
-		line->line = (char *)malloc(size - prev);
-		line->size = size - prev;
-		strncpy(line->line, &lines[prev], size - prev);
-		printf("->%s<-\n", line->line);
-		push_char_array(&vlines, line);
-		prev = size;
-	}
-
-	return vlines;
-}
-
-#elif CHARARR == 2
-
-struct CharArray *push_char_array(struct CharArray *vec, unsigned int *vec_len, struct CharArray c) {
-	if (vec == NULL) {
-		vec = (struct CharArray *)malloc(VECBUF * sizeof(struct CharArray));
-		*vec_len = 0;
-	} else if (*vec_len % VECBUF == 0) {
-		vec = (struct CharArray *)realloc(vec, (*vec_len + VECBUF) * sizeof(struct CharArray));
-	}
-	vec[*vec_len] = c;
-	*vec_len += 1;
-	return vec;
-}
-
-struct CharArray *split_lines(char *lines, size_t size) {
-	struct CharArray *vec = NULL;
-	unsigned int vec_len = 0;
-	int prev = 0;
-
-	for (int i=0; i<size-1; i++) {
-		if (lines[i] == '\r' && lines[i+1] == '\n') {
-			struct CharArray line;
-			if (i == prev) {
-				line.line = "";
-				line.size = 0;
-			} else {
-				line.line = (char *)malloc(i - prev);
-				line.size = i - prev;
-				strncpy(line.line, &lines[prev], i - prev);
-			}
-			vec = push_char_array(vec, &vec_len, line);
-			prev = i+2;
-		}
-	}
-	if (prev < size) {
-		struct CharArray line;
-		line.line = (char *)malloc(size - prev);
-		line.size = size - prev;
-		strncpy(line.line, &lines[prev], size - prev);
-		vec = push_char_array(vec, &vec_len, line);
-		prev = size;
-	}
-
-	return vec;
-}
-
 int split_lines2(char *lines, int size, char **buf, int *buf_s) {
 	int prev = 0;
 	int k = 0;
@@ -135,8 +34,6 @@ int split_lines2(char *lines, int size, char **buf, int *buf_s) {
 	}
 	return k;
 }
-
-#endif
 
 int split_by_words(char *line, int size, char **buf, int *buf_s) {
 	int k = 0;
--- src/char_array.h
+++ src/char_array.h
@@ -3,42 +3,7 @@
 
 #include <stddef.h>
 
-/** CHARARR
-* 1 -> version 1
-* 2 -> version 2
-*/
-#define CHARARR 2
-#define VECBUF 10
-
-#if CHARARR == 1
-
-struct CharArray {
-	char *line;
-	unsigned int size;
-};
-
-struct VecCharArray {
-	struct CharArray **arr;
-	unsigned int len;
-	unsigned int max_len;
-};
-
-struct VecCharArray new_vec_char_array();
-void push_char_array(struct VecCharArray *vec, struct CharArray *arr);
-struct VecCharArray split_lines(char *lines, size_t size);
-
-#elif CHARARR == 2
-
-struct CharArray {
-	char *line;
-	unsigned int size;
-};
-
-struct CharArray *push_char_array(struct CharArray *vec, unsigned int *vec_len, struct CharArray c);
-struct CharArray *split_lines(char *lines, size_t size);
 int split_lines2(char *lines, int size, char **ret, int *ret_s);
-
-#endif
 
 int split_by_words(char *line, int size, char **ret, int *ret_s);
 
