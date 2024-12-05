0099341f3d26e16509648efdeaf0316dfc078810
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-05 20:42:03 +0530

    add more test for queue pull

--- test/thread_task_queue_test.c
+++ test/thread_task_queue_test.c
@@ -61,11 +61,31 @@ void test_thread_task_queue_pull() {
 	ASSERT_EQ(NULL, task_ret);
 }
 
+void test_thread_task_queue_pull_muliple() {
+	thread_task_queue_t *queue = thread_task_queue_init(4);
+	thread_task_t *task1 = (thread_task_t *)malloc(sizeof(thread_task_t));
+	task1->callback = NULL;
+	task1->args = NULL;
+	thread_task_t *task2 = (thread_task_t *)malloc(sizeof(thread_task_t));
+	task2->callback = NULL;
+	task2->args = NULL;
+
+	ASSERT_EQ(0, thread_task_queue_add(queue, task1));
+	ASSERT_EQ(0, thread_task_queue_add(queue, task2));
+
+	thread_task_t *task_ret = thread_task_queue_pull(queue);
+	ASSERT_EQ(task1, task_ret);
+	task_ret = thread_task_queue_pull(queue);
+	ASSERT_EQ(task2, task_ret);
+}
+
+
 int main() {
 	test_thread_init();
 	test_thread_task_queue_add_items();
 	test_thread_task_queue_add_overflow();
 	test_thread_task_queue_overflow2();
 	test_thread_task_queue_pull();
+	test_thread_task_queue_pull_muliple();
 	return 0;
 }
