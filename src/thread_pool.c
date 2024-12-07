b8ab9dbaa0b242ce04e355146a0214cbb6e3b65e
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-07 20:46:47 +0530

    add more thread pool implementation functions

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -11,7 +11,7 @@ typedef struct ThreadPool {
 	thread_task_queue_t *queue;
 } thread_pool_t;
 
-void *thread_process_job(void *args) {
+void *thread_pool_process_job(void *args) {
 	thread_pool_t *pool = (thread_pool_t *)args;
 	while(true) {
 		pthread_mutex_lock(&pool->lock);
@@ -24,12 +24,20 @@ void *thread_process_job(void *args) {
 	}
 }
 
+void *thread_pool_add_job(thread_pool_t *pool, void *args) {
+	pthread_mutex_lock(&pool->lock);
+	thread_task_t *task = thread_task_queue_pull(pool->queue);
+	pthread_mutex_unlock(&pool->lock);
+	task->callback(task->args);
+	return NULL;
+}
+
 void thread_pool_init() {
 	thread_pool_t *pool = (thread_pool_t *)malloc(sizeof(thread_pool_t));
 	pool->queue = thread_task_queue_init(128);
 	pthread_t thread_ids[THREAD_COUNT];
 	for (int i=0; i<THREAD_COUNT; i++) {
-		pthread_create(&thread_ids[i], NULL, thread_process_job, (void *)pool);
+		pthread_create(&thread_ids[i], NULL, thread_pool_process_job, (void *)pool);
 	}
 	for (int i=0; i<THREAD_COUNT; i++) {
 		pthread_join(thread_ids[i], NULL);
