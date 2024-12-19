6c834bdbf541c1f1ab11cae5d7b45aad38828a40
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-19 17:04:49 +0530

    update assert error

--- test/utils.h
+++ test/utils.h
@@ -5,21 +5,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define ASSERT_EQ_STR(a, b)                                                    \
-	if (strlen(a) < strlen(b)) {                                               \
-		fprintf(stderr,                                                        \
-				"Assert failed: %s, %s\nactual: \"%s\", expected: \"%s\"\n",   \
-				#a, #b, a, b);                                                 \
-		fprintf(stderr,                                                        \
-				"String Length: %s, %s\nactual: \"%ld\", expected: \"%ld\"\n", \
-				#a, #b, strlen(a), strlen(b));                                 \
-		exit(-1);                                                              \
-	}                                                                          \
-	if (strncmp(a, b, strlen(b)) != 0) {                                       \
-		fprintf(stderr,                                                        \
-				"Assert failed: %s, %s\nactual: \"%s\", expected: \"%s\"\n",   \
-				#a, #b, a, b);                                                 \
-		exit(-1);                                                              \
+#define ASSERT_EQ_STR(a, b) \
+	if (strncmp(a, b, strlen(b)) != 0) { \
+		fprintf(stderr, "Assert failed: %s:%d in function: %s for " \
+			  "expression ASSERT_EQ_STR(%s, %s) values " \
+			  "expected: \"%s\", actual: \"%s\"\n", \
+			__FILE__, __LINE__, __FUNCTION__, #a, #b, a, b); \
+		exit(-1); \
 	}
 
 #define ASSERT_NEQ(a, b)                                                       \
@@ -31,8 +23,8 @@
 
 #define ASSERT_EQ(a, b) \
 	if ((long)a != (long)b) { \
-		fprintf(stderr, "Assert failed: %s:%d in function: %s for" \
-			  "expression ASSERT_EQ(%s, %s) values" \
+		fprintf(stderr, "Assert failed: %s:%d in function: %s for " \
+			  "expression ASSERT_EQ(%s, %s) values " \
 			  "expected: %ld, actual: %ld\n", \
 			__FILE__, __LINE__, __FUNCTION__, #a, #b, (long)a, \
 			(long)b); \
