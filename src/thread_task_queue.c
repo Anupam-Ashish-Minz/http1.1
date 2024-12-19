98dc60b8584be7b07a32da3786b345f32e7b6580
Anupam Ashish Minz <anupamminz300am@gmail.com>
2024-12-19 11:26:14 +0530

    revamp task queue

--- src/thread_task_queue.c
+++ src/thread_task_queue.c
@@ -13,59 +13,34 @@ thread_task_queue_t *thread_task_queue_init(int size) {
 
 int thread_task_queue_add(thread_task_queue_t *queue, void *(*callback)(void *args), void *args) {
 	thread_task_t *task = (thread_task_t *)malloc(sizeof(thread_task_t));
-	task->callback = callback;
-	task->args = args;
-
 	if (queue->front == -1 && queue->back == -1) {
-		queue->front = 0;
 		queue->back = 0;
+		queue->front = 0;
 		queue->tasks[queue->back] = task;
-		++queue->back;
 		return 0;
 	}
-	if (queue->front == ((queue->back) % queue->size)) {
-		fprintf(stderr, "Task Queue Error: Failed to add new values to queue, queue is full\n"
-			"Queue front: %d, Queue back: %d, Queue size: %d\n",
-			queue->front, queue->back, queue->size);
+	if ((queue->back + 1) % queue->size == queue->front) {
+		fprintf(stderr, "Queue is Full\n");
 		return -1;
-	}
-	if (queue->front <= queue->back) {
+	} else {
+		queue->back = (queue->back + 1) % queue->size;
 		queue->tasks[queue->back] = task;
-		++queue->back;
-		if (queue->back >= queue->size) {
-			queue->back = queue->back % queue->size;
-		}
 		return 0;
 	}
-	if (queue->front > queue->back) {
-		queue->tasks[queue->back] = task;
-		++queue->back;
-		if (queue->front == ((queue->back - 1) % queue->size)) {
-			fprintf(stderr, "Task Queue Error: Failed to add new values to queue, queue is full\n"
-				"Queue front: %d, Queue back: %d, Queue size: %d\n",
-				queue->front, queue->back, queue->size);
-			return -1;
-		}
-		return 0;
-	}
-	return -1;
 }
 
 thread_task_t *thread_task_queue_pull(thread_task_queue_t *queue) {
-	if (queue->front < queue->back) {
-		thread_task_t *task = queue->tasks[queue->front];
-		++queue->front;
-		return task;
-	}
-	if (queue->front >= queue->size) {
-		queue->front = 0;
-	}
-	if (queue->front == queue->back) {
-		++queue->back;
+	if (queue->front == -1 && queue->back == -1) {
 		return NULL;
 	}
-	thread_task_t *task = queue->tasks[queue->front];
-	++queue->front;
-	return task;
+	if (queue->front == queue->back) {
+		thread_task_t *task = queue->tasks[queue->front];
+		queue->front = -1;
+		queue->back = -1;
+		return task;
+	} else {
+		thread_task_t *task = queue->tasks[queue->front];
+		queue->front = (queue->front + 1) % queue->size;
+		return task;
+	}
 }
-
